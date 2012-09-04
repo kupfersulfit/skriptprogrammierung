f58b0d384df035ac2a779ccf37832e9c68a64b78
Kirstin <kirstin.schubert@fh-aachen.de>
2012-09-04 15:50:20 +0200

    Klassen in php
    ohne getter und setter bisher

---
+++ web/lib/objects.php
@@ -0,0 +1,65 @@
+<?php
+class KUNDE {
+	// Member-Variablen
+	private $id;
+	private $name;
+	private $vorname;
+	private $strasse;
+	private $plz;
+	private $zusatz;
+	private $email;
+	private $passwort;
+	private $registriertseit;
+	// Methoden
+	public function __construct($new_var = ??){
+		$this->var = $new_var;
+	}
+	public function setPasswort(){
+	
+	}
+}
+
+class ARTIKEL {
+	// Member-Variablen
+	private $id;
+	private $name;
+	private $beschreibung;
+	private $bildpfad;
+	private $veroeffentlicht;
+	private $verfuegbar;
+	private $kategorieid;
+	private $preis;
+	private $seit;
+	// Methoden
+	public function __construct($new_var = ??){
+		$this->var = $new_var;
+	}
+	public function setBeschreibung() {
+		
+	}
+}
+
+class WARENKORB {
+	// Member-Variablen
+	private $artikel_feld;
+	private $menge;
+	private $summe;
+	// Methoden
+	public function __construct($new_var = ??){
+		$menge = 0;
+		$artikel_feld = new ARTIKEL();
+	}
+	public function setAtrikelFeld($anzahl){	
+		//$menge += anzahl;
+		//$this->artikel_feld['name'] = anzahl;
+	}
+}
+
+
+
+//$obj = new SimpleClass(?Testwert?); // neue Instanz der Klasse
+//$obj->displayVar(); // Aufruf der Methode
+
+
+
+?>
