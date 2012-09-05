ed3c8eaa2b8141a1fb132513a3dc19b8b07b0def
tj <thorsten.jerosch@rwth-aachen.de>
2012-09-05 09:33:30 +0200

    grobe struktur des controller

--- web/lib/controller.php
+++ web/lib/controller.php
@@ -1,13 +1,30 @@
 <?php
-	if(!isset($_GET['action']){
-		//TODO error
-		exit(1);
-	}
+    session_start();
 
-	switch($_GET['action']){
-		case '':
-			break;
-		default:
-			//TODO error
-	}
+    if(!isset($_REQUEST['action'])){
+        echo json_encode(array('error' => 'action parameter missing'));
+        die();
+    }
+
+    //public actions
+    switch($_REQUEST['action']){
+        case 'holeArtikel':
+            exit();
+        case 'login':
+            exit();
+    }
+
+    //private actions
+    if(!isset($_SESSION['kunde'])){
+        echo json_encode(array('error' => 'unknown action or you need to be logged in'));
+        die();
+    }
+
+    switch($_REQUEST['action']){
+        case 'holeKundenDetails':
+            exit();
+        case 'logout':
+            exit();
+        default:        
+    }
 ?>
