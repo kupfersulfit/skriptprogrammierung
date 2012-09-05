e54f17b6d7facb0e03f412718592deebd7aa3c9f
tj <thorsten.jerosch@rwth-aachen.de>
2012-09-05 12:31:12 +0200

    controller aktualisiert, klassendiagramm angepasst

--- web/lib/controller.php
+++ web/lib/controller.php
@@ -1,5 +1,9 @@
 <?php
     session_start();
+    include_once('controller_functions.php');
+    if(!isset($_SESSION['kunde'])){ //session und TODO warenkorb erstellen
+        $_SESSION['kunde'] = "gast";
+    }
 
     if(!isset($_REQUEST['action'])){
         echo json_encode(array('error' => 'action parameter missing'));
@@ -8,7 +12,11 @@
 
     //public actions
     switch($_REQUEST['action']){
-        case 'holeArtikel':
+        case 'zeigeArtikel':
+            exit();
+        case 'sucheArtikel':
+            exit();
+        case 'aktualisiereWarenkorb':
             exit();
         case 'login':
             exit();
@@ -16,15 +24,20 @@
 
     //private actions
     if(!isset($_SESSION['kunde'])){
-        echo json_encode(array('error' => 'unknown action or you need to be logged in'));
+        echo json_encode(array('error' => 'unknown action or not logged in'));
         die();
     }
 
     switch($_REQUEST['action']){
-        case 'holeKundenDetails':
+        case 'holeKunde':
             exit();
         case 'logout':
             exit();
-        default:        
+        case 'erstelleArtikel':
+            exit();
+        case 'loescheArtikel':
+            exit();
+        default:   
+            echo json_encode(array('error' => 'unknown action'));     
     }
 ?>
