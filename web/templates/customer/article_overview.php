ec657b7eee0b0673674b95b2d3c54ac71fd36c7d
Kartoffelsalat <lukas.bonzelett@traveltainment.de>
2012-09-06 15:43:10 +0200

    template v 0.5

--- web/templates/customer/article_overview.php
+++
@@ -1,8 +0,0 @@
-<section id="articleList">
-    
-</section>
-<section id="mini_shopping_card">
-    <img id="shopping_card_logo" src="" alt="shopping card" width="" height=""/>
-    <div id="basket">
-    </div>
-</section>
