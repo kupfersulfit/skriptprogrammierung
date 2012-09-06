e0f665a68df401509e7ac328e244f79bdf53b3b8
Kartoffelsalat <lukas.bonzelett@traveltainment.de>
2012-09-06 14:07:33 +0200

    template v 0.5

--- web/templates/header.php
+++ web/templates/header.php
@@ -6,6 +6,9 @@
             <link type="text/css" rel="stylesheet" href="css/style_article_overview.css" />
             <script type="text/javascript" src="js/jquery-1.8.0.min.js" ></script>
             <script type="text/javascript" src="js/main.js" ></script>
+            <script type="text/javascript" src="js/article.js" ></script>
+            <script type="text/javascript" src="js/ajax.js" ></script>
+            <script type="text/javascript" src="js/ready.js" ></script>
         </head>
         <body>
             <header>
@@ -20,13 +23,22 @@
                         <li id="homeTab" class="active">
                             <img id="home" src="media/home.png" height="70" width="100" />
                         </li>
+                        <?php
+                        if (TRUE) {//Admin
+                            ?>
+                            <li id="adminTab">
+                                <img id="admin" alt="Admin" src="" height="70" width="100" />
+                            </li>
+                            <?php
+                        }
+                        ?>
                     </ul>
                 </nav>
                 <div class="clear"></div>
                 <div id="container">
                     <div id="loginContainer" >
-                        <p><label for="email" >email</label><input type="text" id="email" name="email"/></p>
-                        <p><label for="password" >password</label><input type="password" id="password" name="password"/></p>
+                        <p><label for="email" >email</label><input type="text" id="email" name="email" autocomplete="off"/></p>
+                        <p><label for="password" >password</label><input type="password" id="password" name="password" autocomplete="off"/></p>
                         <p><input type="button" id="loginButton" value="login"/></p>
                     </div>
                     <div id="register_login"><a id="registerLink">register</a></div>
@@ -47,8 +59,8 @@
                         <p><input type="text" id="registerPassword" name="registerPassword"/></p>
                         <p><label for="validPassword" >password validation</label></p>
                         <p><input type="text" id="validPassword" name="validPassword"/></p>
-                        <p><label for="edition" >edition</label></p>
-                        <p><textarea id="edition"></textarea></p>
+                        <p><label for="addition" >addition</label></p>
+                        <p><textarea id="addition"></textarea></p>
                         <p><input id="registerButton" type="button" value="register" /></p>
                     </div>
                 </div>
