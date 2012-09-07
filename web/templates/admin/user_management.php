e6ecfc08ad6351d3740caae2d4523d4c42480694
Kirstin <kirstin.schubert@fh-aachen.de>
2012-09-07 11:30:31 +0200

    wie kommt man an die Dtaen beispiel

--- web/templates/admin/user_management.php
+++ web/templates/admin/user_management.php
@@ -9,9 +9,15 @@
 	<head>
 		<title>st@pleware</title>
 		<link type="text/css" rel="stylesheet" href="style_admin.css"/>
-		<script type="text/javascript" src="js/jquery-1.8.0.min.js" ></script>
-        <script type="text/javascript" src="js/ajax_admin.js" ></script>
-
+		<script type="text/javascript" src="../../js/jquery-1.8.0.min.js" ></script>
+        <script type="text/javascript" src="ajax_admin.js" ></script>
+		<script type="text/javascript">
+			$(document).on('click', "input[name='send']", 
+				function() {
+					var id = this.attributes.getNamedItem('id');
+					var bla =2;
+				});
+		</script>
 	</head>
 	<body>
 
@@ -19,15 +25,17 @@
 
 <!-- alle Kunden ausgeben -->
 <form method="post">
-	<table border=1>
+	<p onclick='getAlleKunden()'>ladeKunden</p>
+	<table border=1 id="KundenTable">
+		<thead>
 		<tr>
 			<th>ID</th><th>Name</th><th>Vorname</th><th>Straße</th><th>PLZ</th>
 			<th>Zusatz</th><th>email</th><th>Passwort</th><th>Registriert Seit</th><th>Ändern</th>
 		</tr>
-		<tr>
-			<!-- über alle Kunden iterieren-->
-			
-		</tr>
+		</thead>
+		<tbody id='KundenBody'>
+			<tr><td></td></tr>
+		</tbody>
 	</table>
 </form>
 
@@ -36,25 +44,6 @@
 
 <?
 /*
-        jQuery.ajax({
-            type : 'POST',
-            url : 'lib/controller.php', 
-            data : {
-                'action' : 'login',
-                'email' : jQuery('#email').val(),
-                'passwort' : jQuery('#password').val()
-            },
-            dataType : 'jsonp',
-            success : function (json) {
-                console.debug(json);
-                jQuery('#email, #password').css('border-color','#FFFFFF');
-                jQuery('#email').val('');
-            },
-            error : function (json) {
-                console.debug(json);
-            }
-        });
-		
 		
 function getAdminContent(pageName) {
     jQuery.ajax({
