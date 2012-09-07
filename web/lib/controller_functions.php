aff13b70948c2cd5e9b5b18ebb3a4157eb2de99d
tj <thorsten.jerosch@rwth-aachen.de>
2012-09-07 14:47:26 +0200

    kategorienamen auch bei action:zeigeArtikel korrekt

--- web/lib/controller_functions.php
+++ web/lib/controller_functions.php
@@ -15,6 +15,12 @@
             err("no existing article");
         }else{
             for($i = 0; $i < count($artikelArray); $i++){ //convert object to assoc array
+                //TODO kategorie ersetzen wie unten
+                //kategorienamen aus der db holen und eintragen
+                $kategorie = $artikelArray[$i]->getKategorieId();
+                $kategorie = $_SESSION["model"]->holeKategorie($kategorie);
+                $artikelArray[$i]->setKategorieId($kategorie->getName());
+                //objekt in assoz array umwandeln
                 $artikelArray[$i] = $artikelArray[$i]->assoc();
             }
             echo json_encode($artikelArray);
@@ -30,7 +36,7 @@
             for($i = 0; $i < count($artikelArray); $i++){ //convert object to assoc array
                 //kategorienamen aus der db holen und eintragen
                 $kategorie = $artikelArray[$i]->getKategorieId();
-                $kategorie = $_SESSION["model"]->holeKategorie($kategorie); //TODO mit id aus db holen
+                $kategorie = $_SESSION["model"]->holeKategorie($kategorie); 
                 $artikelArray[$i]->setKategorieId($kategorie->getName());
                 //objekt in assoz array umwandeln
                 $artikelArray[$i] = $artikelArray[$i]->assoc();
