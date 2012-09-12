65420a1b7db81e9c8d100184a23300ec3e42a2fa
tj <thorsten.jerosch@rwth-aachen.de>
2012-09-12 11:27:37 +0200

    debug ausgabe geloescht

--- web/lib/controller_functions.php
+++ web/lib/controller_functions.php
@@ -225,7 +225,6 @@
     */
     function aktualisiereKunde($kunde){
         $kunde = json_decode($kunde, true);
-        print_r($kunde);
         //hole aktuelles pw aus der db 
         $alterkunde = $_SESSION['model']->holeKundeMitId($kunde['id']);
         //vermeide dass das pw geloescht wird
